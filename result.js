744bebb4118e8507db7fe46432e808be74f1e7c1
a1234561524 <35169221+a1234561524@users.noreply.github.com>
2021-11-13 12:55:06 +0800

    Add files via upload

--- result.js
+++ result.js
@@ -1,22 +1,22 @@
 "use strict";
-function getAuthorizationHeader() {
-  //  填入自己 ID、KEY 開始
-  let AppID = "2af5643ac6c948c4b206225d87f27506";
-  let AppKey = "5ewryF_mUSxIGukfP-b5CyCnE-o";
-  //  填入自己 ID、KEY 結束
-  let GMTString = new Date().toGMTString();
-  let ShaObj = new jsSHA("SHA-1", "TEXT");
-  ShaObj.setHMACKey(AppKey, "TEXT");
-  ShaObj.update("x-date: " + GMTString);
-  let HMAC = ShaObj.getHMAC("B64");
-  let Authorization =
-    'hmac username="' +
-    AppID +
-    '", algorithm="hmac-sha1", headers="x-date", signature="' +
-    HMAC +
-    '"';
-  return { Authorization: Authorization, "X-Date": GMTString };
-}
+// function getAuthorizationHeader() {
+//   //  填入自己 ID、KEY 開始
+//   let AppID = "2af5643ac6c948c4b206225d87f27506";
+//   let AppKey = "5ewryF_mUSxIGukfP-b5CyCnE-o";
+//   //  填入自己 ID、KEY 結束
+//   let GMTString = new Date().toGMTString();
+//   let ShaObj = new jsSHA("SHA-1", "TEXT");
+//   ShaObj.setHMACKey(AppKey, "TEXT");
+//   ShaObj.update("x-date: " + GMTString);
+//   let HMAC = ShaObj.getHMAC("B64");
+//   let Authorization =
+//     'hmac username="' +
+//     AppID +
+//     '", algorithm="hmac-sha1", headers="x-date", signature="' +
+//     HMAC +
+//     '"';
+//   return { Authorization: Authorization, "X-Date": GMTString };
+// }
 
 let contain,
   count = 0,
@@ -100,10 +100,10 @@ let search = function (type, city) {
   page = 1;
   axios
     .get(
-      `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}${city}?$format=JSON`,
-      {
-        headers: getAuthorizationHeader(),
-      }
+      `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}${city}?$format=JSON`
+      // {
+      //   headers: getAuthorizationHeader(),
+      // }
     )
     .then(function (response) {
       contain = response.data;
